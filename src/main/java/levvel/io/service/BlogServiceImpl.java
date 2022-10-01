3b1582aaeb7230e685ac0d977597005de27503be
samiyusuf <samiyusuf@tamu.edu>
2022-10-01 14:20:48 -0500

    Added Comment Feature

--- src/main/java/levvel/io/service/BlogServiceImpl.java
+++ src/main/java/levvel/io/service/BlogServiceImpl.java
@@ -2,8 +2,11 @@ package levvel.io.service;
 
 import levvel.io.data.BlogRepository;
 import levvel.io.model.Blog;
+import levvel.io.model.Comment;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 @AllArgsConstructor
@@ -19,5 +22,37 @@ public class BlogServiceImpl implements BlogService {
     @Override
     public Blog getBlog(String id) {
         return blogRepository.findById(id).orElseGet(null);
+    }
+
+    /*
+     *  Create a new comment for a post with passed {id}
+    */
+
+    @Override
+    public void addComment(Comment comment, String id){
+        Blog blog = getBlog(id);
+        if(blog != null){
+            blog.addNewComment(comment);
+            blogRepository.save(blog);
+        }
+        else{
+            System.out.println("Invalid Blog ID");
+        }
+    }
+
+    /*
+     *  Return all comments for a post with passed {id}
+    */
+
+    @Override
+    public ArrayList<Comment> getComments(String id){
+        Blog blog = getBlog(id);
+        if(blog != null){
+            return blog.getallComments();
+        }
+        else{
+            System.out.println("Invalid Blog ID");
+            return null;
+        }
     }
 }
